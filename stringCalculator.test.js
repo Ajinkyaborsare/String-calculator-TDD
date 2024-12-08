208f327f02f7596caeb1cf34457d36c1ae6a6f61
AjinkyaBorsare <bajinkya4@gmail.com>
2024-12-08 11:17:38 +0530

    Support both comma and newline as delimiters

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -15,3 +15,8 @@ const { add } = require('./stringCalculator');
     test('should return the sum of multiple comma-separated numbers', () => {
         expect(add("1,2,3")).toBe(6);
     });
+
+    test('should handle newlines as delimiters', () => {
+        expect(add("1\n2,3")).toBe(6);
+    });
+    
