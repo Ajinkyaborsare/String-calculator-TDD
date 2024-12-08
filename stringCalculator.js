12cdbb314b45bfb5c2c7375eb5c15b25139008cb
AjinkyaBorsare <bajinkya4@gmail.com>
2024-12-08 11:26:39 +0530

    Support Custom Delimiters

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,6 +1,17 @@
 function add(numbers) {
     if (!numbers) return 0;
-    return parseInt(numbers).split(/,|\n/).reduce((sum, n) => sum + parseInt(n), 0); 
+
+    let delimiter = /,|\n/; 
+    if (numbers.startsWith("//")) {
+        const parts = numbers.split("\n", 2); 
+        delimiter = new RegExp(parts[0].slice(2)); 
+        numbers = parts[1]; 
+    }
+
+
+    return numbers
+    .split(delimiter)
+    .reduce((sum, n) => sum + parseInt(n), 0); 
 }
 
 module.exports = { add };
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -19,4 +19,9 @@ const { add } = require('./stringCalculator');
     test('should handle newlines as delimiters', () => {
         expect(add("1\n2,3")).toBe(6);
     });
+
+    test('should support custom delimiters', () => {
+        expect(add("//;\n1;2")).toBe(3);
+    });
+    
     
