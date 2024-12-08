c6102e090d9003e7c369f6cb6b3f068b300a4cb6
AjinkyaBorsare <bajinkya4@gmail.com>
2024-12-08 14:59:04 +0530

    failing test for single number input  logic to handle single number

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -4,4 +4,8 @@ test('should return 0 for an empty string', () => {
     expect(add("")).toBe(0);
 });
 
+test('should return the number itself if only one number is provided', () => {
+    expect(add("5")).toBe(5);
+});
+
 
